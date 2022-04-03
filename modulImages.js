e9f4f7ebc020c47cd2d725331c60adf96274d7a7
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-04-03 13:22:06 +0300

    fix1

--- modulImages.js
+++ modulImages.js
@@ -1,41 +1,12 @@
-// images
-const imagesNature = [
-    {
-        url: "https://cameralabs.org/media/camera/avgust/26avgust/53_7fa35563b639e7ce500ed9325afe6f63.jpg",
-        title: 'Безутешный лес'
-    },
-    {
-        url: "https://cameralabs.org/media/camera/avgust/26avgust/53_e3583ce58fccac820ec4da6b53732ac5.jpg",
-        title: 'Стадия пробуждения'
-    },
-    {
-        url: "https://cameralabs.org/media/camera/avgust/26avgust/53_373e7f9ceca112e1f35ca2b38a25da91.jpg",
-        title: 'Уэльс'
-    },
-    {
-        url: "https://cameralabs.org/media/camera/avgust/26avgust/53_d3d67c699da1158f0abdb15a03c0edab.jpg",
-        title: 'Край Земли'
-    },
-    {
-        url: "https://cameralabs.org/media/camera/avgust/26avgust/53_f3f8a62baafd6d865d23c713c74cbca7.jpg",
-        title: 'Вода для Богов'
-    },
-    {
-        url: "https://cameralabs.org/media/camera/avgust/26avgust/53_e55396a7ba0f55dc6fae2c53e4b6feaa.jpg",
-        title: 'Исландия'
-    },
-    {
-        url: "https://cameralabs.org/media/camera/avgust/26avgust/53_ab9bdbccb7f18114493f5f8589e3261f.jpg",
-        title: 'Радуга в Альпах'
-    },
+import imagesNature from './images.js';
+import options from './parameters.js';
 
-]
 
 let titleH3 = document.querySelector('.super-h3');
 titleH3.innerHTML += `${imagesNature.length} красивейших мест на планете`;
 
 function mySlider(options) {
-
+    console.log(imagesNature)
     if (!imagesNature && !imagesNature.length) return;
 
     options = options || {
@@ -192,20 +163,6 @@ function mySlider(options) {
     }
 }
 
-const options = {
-    autoLoop: true,
-    arrowSlider: true,
-    switchSlider: true,
-    randomImage: true,
-    dotsImage: true,
-    titleImage: true,
-    autoPlayLoop: true,
-    timeAutoPlay: 6000,
-    content: function () {
-        button = true;
-        text = true;
-    }
-}
 
 
 document.addEventListener('DOMContentLoaded', function () {
